635354a7d8e58ab9b24157af1c637595161a1b43
Mauricio Schell <mauricio@mauricio-IS-1522.(none)>
2011-10-03 17:59:47 -0300

    remove  default rails page

--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -1,29 +1,34 @@
 require 'spec_helper'
 
 describe PagesController do
- render_views
 
-  describe "GET 'home'" do
+ describe "GET 'home'" do
     it "should be successful" do
       get 'home'
       response.should be_success
     end
-
   end
 
-  describe "GET 'contact'" do
+describe "GET 'about'" do
+    it "should be successful" do
+      get 'about'
+      response.should be_success
+    end
+  end
+
+describe "GET 'contact'" do
     it "should be successful" do
       get 'contact'
       response.should be_success
     end
   end
 
-  describe "GET 'about'" do
+
+  describe "GET 'help'" do
     it "should be successful" do
-	get 'about'
-	response.should be_success
+      get 'help'
+      response.should be_success
     end
   end
-
 
 end
